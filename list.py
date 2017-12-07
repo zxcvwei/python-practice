94862397a413cd58beb4fd8ee2c623b9a18ff238
zxcvwei <104021031@live.asia.edu.tw>
2017-12-07 14:16:42 +0800

    Add files via upload

---
+++ list.py
@@ -0,0 +1,6 @@
+﻿name = ["elephant", "rabbit", "lion"]
+number = [1, 2, 3]
+score = [85, 43, 99]
+
+for person in range(0,len(name)):
+  print (str(name[person]), str(number[person]), str(score[person]))
